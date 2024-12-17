9fe506ad2412a8c032db4bdf2cdb4b4fe31bbe72
Giselle276 <gisellemaccha27@gmail.com>
2024-12-17 12:20:58 +0100

    Update first_word.c

--- Exam_Rank02/Level1/first_word.c
+++ Exam_Rank02/Level1/first_word.c
@@ -33,24 +33,3 @@ int	main (int argc, char *argv[])
 	write(1, "\n", 1);
 	return (0);
 }
-
-int main (int argc, char *argv[])
-{
-	int	i;
-	char	*str;
-
-	i = 0;
-	str = argv[1];
-	if (argc == 2)
-	{
-		while (str[i] == 32 || str[i] == 9)
-		i++;
-		while ((str[i] != 32 && str[i] != 9 ) && str[i] != '\0')
-		{
-			write(1, &str[i], 1);
-			i++;
-		}
-	}
-	write(1,"\n", 1);
-	return(0);
-}
