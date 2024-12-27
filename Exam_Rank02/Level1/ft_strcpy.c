68835017e76a77c5349838603c6bfb24434bc00d
Giselle276 <gisellemaccha27@gmail.com>
2024-12-27 13:15:02 +0100

    Update ft_strcpy.c

--- Exam_Rank02/Level1/ft_strcpy.c
+++ Exam_Rank02/Level1/ft_strcpy.c
@@ -23,23 +23,3 @@ char	*ft_strcpy(char *s1, char *s2)
 	s1[i] = '\0';
 	return (s1);
 }
-#include <stdio.h>
-
-int	main(int argc, char *argv[])
-{
-	char	*str;
-	char	*dest;
-
-	str = argv[1];
-	dest = argv[2];
-	if (argc != 3)
-		printf("Error\n");
-	else
-	{
-		printf("Cadena:%s\n", str);
-		ft_strcpy(dest, str);
-		printf("Resultado: %s\n",dest);
-	}
-	return (0);
-}
-
