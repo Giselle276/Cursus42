7a1956a53d8fe1318fa614da900d5889574d9987
Giselle276 <gisellemaccha27@gmail.com>
2024-12-27 13:16:25 +0100

    Update rev_print.c

--- Exam_Rank02/Level1/rev_print.c
+++ Exam_Rank02/Level1/rev_print.c
@@ -29,13 +29,3 @@ char	*rev_print(char *str)
 	write(1, "\n", 1);
 	return (str);
 }
-
-int main(void)
-{
-  rev_print("Hello world");
-  write (1, "\n", 1);
-  rev_print("tnirp esreveR");
-  write (1, "\n", 1);
-  rev_print("");
-  write (1, "\n", 1);
-}
